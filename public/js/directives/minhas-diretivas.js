55966a33258f447ef2bafcc62aa59cd4abac9acc
Marcio Carvalho <mhn.carvalho@gmail.com>
2016-02-24 23:35:37 -0300

    exercicio capitulo 5 - aplicando single page application "SPA"

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -1,15 +1,25 @@
 angular.module('minhasDiretivas', [])
 .directive('meuPainel', function() {
-	    var ddo = {};
-
-        ddo.restrict = "AE";
+		var ddo = {};
+		ddo.restrict = "AE";
 		ddo.transclude = true;
 
-        ddo.scope = {
-        	titulo : '@'
-        };
+		ddo.scope = {
+			titulo : '@'
+		};
 
- 		ddo.templateUrl = '/js/directives/meu-painel.html';
+		ddo.templateUrl = '/js/directives/meu-painel.html';
+		return ddo;
+})
+.directive('minhaFoto', function() {
+		var ddo = {};
+		ddo.restrict = 'AE';
 
-        return ddo;
+		ddo.scope = {
+			titulo : '@',
+			url : '@'
+		};
+
+		ddo.templateUrl = '/js/directives/minha-foto.html';
+		return ddo;
 });
